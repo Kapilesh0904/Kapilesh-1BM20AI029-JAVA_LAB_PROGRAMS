05dd58b7bb7361fc0cd34e79f06ab6cbf4b10925
Kapilesh0904 <95362556+Kapilesh0904@users.noreply.github.com>
2022-12-29 20:27:28 +0530

    Added the codes for all the string questions

---
+++ src/Strings/Words.java
@@ -0,0 +1,21 @@
+package Strings;
+import java.util.*;
+
+public class Words 
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence :");
+        String sentence = sc.nextLine();
+        int word_count = 0;
+        for(int i=0;i<sentence.length();i++)
+        {
+            if(sentence.charAt(i) == ' ')
+                word_count+=1;
+        }
+        System.out.println("No. of words = " + (word_count+1));
+        System.out.println("Sentence in upper case : " + sentence.toUpperCase());
+        sc.close();
+    }
+}
